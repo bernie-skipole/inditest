c47f5270c847535489925bebdcf89692194e4110
Bernard Czenkusz <bernie@skipole.co.uk>
2024-09-07 15:22:39 +0100

    serve with logging added

--- remotes/led1.py
+++ remotes/led1.py
@@ -1,11 +1,9 @@
 # /// script
 # requires-python = ">=3.10"
 # dependencies = [
-#     "indipyclient",
 #     "indipydriver",
 # ]
 # ///
-
 
 """Driver and server to control a (simulated) LED
    with devicename led1
@@ -13,6 +11,9 @@
    to test connections from serve_remotes.py enabling
    multiple connected servers to be run on a single machine"""
 
+## ignore these, used for development
+#import sys
+#sys.path.insert(0, "/home/bernard/git/indipydriver")
 
 import asyncio
 import indipydriver as ipd
--- remotes/serve_with_logging.py
+++ remotes/serve_with_logging.py
@@ -1,11 +1,27 @@
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "indipydriver",
+# ]
+# ///
+
 
 """Set to connect to two remote servers led1.py and led2.py
    Rather than using remote machines, these three services are all on
-   one machine using different ports"""
+   one machine using different ports
+
+   This adds logging, so a logfile is created, for the remote
+   link to led1."""
 
 ## ignore these, used for development
 #import sys
 #sys.path.insert(0, "/home/bernard/git/indipydriver")
+
+import logging
+led1logger = logging.getLogger('indipydriver.remotelink')
+led1fh = logging.FileHandler("logled1.log")
+led1logger.addHandler(led1fh)
+led1logger.setLevel(logging.DEBUG)
 
 
 import asyncio
@@ -17,7 +33,7 @@ if __name__ == "__main__":
     server = ipd.IPyServer(host="localhost", port=7624, maxconnections=5)
 
     # connect to two remote servers
-    server.add_remote(host="localhost", port=7625)
+    server.add_remote(host="localhost", port=7625, debug_enable=True)
     server.add_remote(host="localhost", port=7626)
     print(f"Running {__file__}")
     asyncio.run(server.asyncrun())
