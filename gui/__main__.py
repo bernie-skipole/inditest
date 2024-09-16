a2dbcb7b98acd162c9b9bc6b1db4351661f348c0
Bernard Czenkusz <bernie@skipole.co.uk>
2024-09-16 12:37:20 +0100

    Working on QueClient

--- gui/__main__.py
+++ gui/__main__.py
@@ -1,6 +1,5 @@
 
-import asyncio, queue, threading, time, collections
-
+import asyncio, queue, threading, collections
 
 import sys
 sys.path.insert(0, "/home/bernard/git/indipyclient")
@@ -13,15 +12,22 @@ from .guiclient import rungui
 EventItem = collections.namedtuple('EventItem', ['eventtype', 'devicename', 'vectorname', 'timestamp', 'snapshot'])
 
 
-class GuiClient(ipc.IPyClient):
+class QueClient(ipc.IPyClient):
 
     """Overrides IPyClient
-       On receiving an event, appends a snapshot into rxque
-       Which will pass to Guiclient"""
+       On receiving an event, appends a snapshot into self.rxque
+       Gets contents of self.txque """
+
+
+    def __init__(self, indihost="localhost", indiport=7624):
+        super().__init__(indihost, indiport)
+        # create queue where client will put snapshots on receiving data
+        self.rxque = queue.SimpleQueue()
+        # create queue of things to send
+        self.txque = queue.SimpleQueue()
 
     async def rxevent(self, event):
-        """Add event type and snapshot to rxque"""
-        rxque = self.clientdata['rxque']
+        """Add event type and snapshot to self.rxque"""
         if event.eventtype == "Define":
             item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, event.vector.snapshot())
         elif event.eventtype == "DefineBLOB":
@@ -42,41 +48,40 @@ class GuiClient(ipc.IPyClient):
             item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, event.vector.snapshot())
         else:
             return
-        while not self._stop:
-            try:
-                rxque.put_nowait(item)
-            except queue.Full:
-                asyncio.sleep(0.01)
-            else:
-                break
+        self.rxque.put(item)
+
 
     async def hardware(self):
-        "Read txque and send data"
-        txque = self.clientdata['txque']
+        """Read self.txque and send data
+           Item passed in the queue should be a tuple or list of (devicename, vectorname, value)
+           where value is normally a membername to membervalue dictionary
+           If value is a string, one of  "Never", "Also", "Only" then an enableBLOB will be sent
+           If the item is None, this indicates the client should shut down"""
         while not self._stop:
             try:
-                item = txque.get_nowait()
+                item = self.txque.get_nowait()
             except queue.Empty:
-                asyncio.sleep(0.02)
+                await asyncio.sleep(0.02)
+                continue
+            if item is None:
+                # A None in the queue is a shutdown indicator
+                self.shutdown()
+                return
+            if len(item) != 3:
+                # invalid item
+                continue            
+            if item[2] in ("Never", "Also", "Only"):
+                await self.send_enableBLOB(item[2], item[0], item[1])
             else:
-                if item is None:
-                    # A None in the queueis a shutdown indicator
-                    self.shutdown()
-                    return
-                await self.send_newVector(item.devicename, item.vectorname, members=item.members):
+                await self.send_newVector(item[0], item[1], members=item[2]):
 
 
 if __name__ == "__main__":
 
-    # create queue where client will put snapshots on receiving data
-    rxque = queue.Queue(maxsize=3)
-    # create queue of things to send
-    txque = queue.Queue(maxsize=3)
-    # create a GuiClient object
-    client = GuiClient(indihost="localhost", indiport=7624, txque = txque, rxque = rxque)
-
+    # create a QueClient object
+    client = QueClient(indihost="localhost", indiport=7624)
     # rungui in its own thread
-    clientapp = threading.Thread(target=rungui, args=(txque, rxque))
+    clientapp = threading.Thread(target=rungui, args=(client.txque, client.rxque))
     clientapp.start()
     print(f"Running {__file__}")
     asyncio.run(client.asyncrun())
