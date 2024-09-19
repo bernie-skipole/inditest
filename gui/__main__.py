94854b891e2e366b33c69d75e011326e22b0b775
Bernard Czenkusz <bernie@skipole.co.uk>
2024-09-19 12:01:28 +0100

    Working on gui

--- gui/__main__.py
+++ gui/__main__.py
@@ -19,54 +19,29 @@ class QueClient(ipc.IPyClient):
        Gets contents of self.txque and transmits updates"""
 
 
-    def __init__(self, indihost="localhost", indiport=7624):
-        super().__init__(indihost, indiport)
-        # create queue where client will put snapshots on receiving data
-        self.rxque = queue.SimpleQueue()
-        # create queue of things to send
-        self.txque = queue.SimpleQueue()
-
-
     async def rxevent(self, event):
-        """Add event type and snapshot to self.rxque"""
+        """Add EventItem to rxque, where an EventItem is a named tuple with attributes:
+           eventtype - one of .....
+           devicename
+           vectorname
+           timestamp
+           snapshot
+           """
         item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, self.snapshot())
-        self.rxque.put(item)
-
-
-    async def optionrxevent(self, event):
-        """Add event type and snapshot to self.rxque"""
-        if event.eventtype == "Define":
-            item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, event.vector.snapshot())
-        elif event.eventtype == "DefineBLOB":
-            item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, event.vector.snapshot())
-        elif event.eventtype == "Set":
-            item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, event.vector.snapshot())
-        elif event.eventtype == "SetBLOB":
-            item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, event.vector.snapshot())
-        elif event.eventtype == "Delete":
-            item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, event.device.snapshot())
-        elif event.eventtype == "Message":
-            if event.devicename is None:
-                # state wide message
-                item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, list(self.messages))
-            else:
-                item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, list(event.device.messages))
-        elif event.eventtype == "TimeOut":
-            item = EventItem(event.eventtype, event.devicename, event.vectorname, event.timestamp, event.vector.snapshot())
-        else:
-            return
-        self.rxque.put(item)
-
+        try:
+            self.clientdata['rxque'].put_nowait(item)
+        except queue.Full:
+            await asyncio.sleep(0.02)
 
     async def hardware(self):
-        """Read self.txque and send data
+        """Read txque and send data to server
            Item passed in the queue should be a tuple or list of (devicename, vectorname, value)
            where value is normally a membername to membervalue dictionary
            If value is a string, one of  "Never", "Also", "Only" then an enableBLOB will be sent
            If the item is None, this indicates the client should shut down"""
         while not self._stop:
             try:
-                item = self.txque.get_nowait()
+                item = self.clientdata['txque'].get_nowait()
             except queue.Empty:
                 await asyncio.sleep(0.02)
                 continue
@@ -85,10 +60,16 @@ class QueClient(ipc.IPyClient):
 
 if __name__ == "__main__":
 
+    # create two queues
+    # rxque giving received data
+    rxque = queue.Queue(maxsize=4)
+    # txque transmit data
+    txque = queue.Queue(maxsize=4)
+
     # create a QueClient object
-    client = QueClient(indihost="localhost", indiport=7624)
+    client = QueClient(indihost="localhost", indiport=7624, txque=txque, rxque=rxque)
     # rungui in its own thread
-    clientapp = threading.Thread(target=rungui, args=(client.txque, client.rxque))
+    clientapp = threading.Thread(target=rungui, args=(txque, rxque))
     clientapp.start()
     print(f"Running {__file__}")
     asyncio.run(client.asyncrun())
--- gui/guiclient.py
+++ gui/guiclient.py
@@ -3,35 +3,91 @@ from tkinter import *
 from tkinter import ttk
 
 
-class FeetToMeters:
+def appframe(root):
+    root.title("Indi Client")
+    root.minsize(600, 400)  # width, height
+    frame = ttk.Frame(root, padding="3 3 3 3")
+    frame.grid(column=0, row=0, sticky=(N, W, E, S))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+    frame.columnconfigure(0, weight=1)
+    frame.rowconfigure(0, weight=1)
+    return frame
 
-    def __init__(self, root):
 
-        root.title("Feet to Meters")
 
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
-       
+class ParentScreen:
+
+    def __init__(self, txque, rxque, root, applicationframe):
+        self.txque = txque
+        self.rxque = rxque
+        self.root = root
+        self.applicationframe = applicationframe
+        self.tframe = self.topframe(applicationframe)
+        self.mainframe = self.middleframe(applicationframe)
+        self.butframe = self.buttonframe(applicationframe)
+
+
+    def topframe(self, applicationframe):
+        frame = ttk.Frame(applicationframe, padding="3 3 12 12", borderwidth=5, relief='groove')
+        frame.grid(column=0, row=0, sticky=(N, W, E, S))
+        return frame
+
+
+    def middleframe(self, applicationframe):
+        frame = ttk.Frame(applicationframe, padding="3 3 12 12", borderwidth=5, relief='groove')
+        frame.grid(column=0, row=1, sticky=(N, W, E, S))
+        return frame
+
+
+    def buttonframe(self, applicationframe):
+        frame = ttk.Frame(applicationframe, padding="3 3 3 3", borderwidth=5)#, relief='groove')
+        frame.grid(column=0, row=2, sticky=(N, W, E, S))
+
+        ttk.Button(frame, text="One").grid(column=1, row=0)
+        ttk.Button(frame, text="Two").grid(column=2, row=0)
+        ttk.Button(frame, text="Three").grid(column=3, row=0)
+        ttk.Button(frame, text="Quit", command=lambda: self.tx_data(None)).grid(column=4, row=0)
+        frame.columnconfigure(0, weight=2)
+        frame.columnconfigure(1, weight=1)
+        frame.columnconfigure(2, weight=1)
+        frame.columnconfigure(3, weight=1)
+        frame.columnconfigure(4, weight=1)
+        frame.columnconfigure(5, weight=2)
+        return frame
+
+
+    def tx_data(self, data):
+        self.txque.put(data)
+        if data is None:
+            # destroy root
+            self.root.destroy()
+
+
+
+
+class MessageScreen(ParentScreen):
+
+    def __init__(self, txque, rxque, root, applicationframe):
+        super().__init__(txque, rxque, root, applicationframe)
         self.feet = StringVar()
-        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
+        feet_entry = ttk.Entry(self.mainframe, width=7, textvariable=self.feet)
         feet_entry.grid(column=2, row=1, sticky=(W, E))
         self.meters = StringVar()
 
-        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
-        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
+        ttk.Label(self.mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
+        ttk.Button(self.mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
 
-        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+        ttk.Label(self.mainframe, text="feet").grid(column=3, row=1, sticky=W)
+        ttk.Label(self.mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+        ttk.Label(self.mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
-        for child in mainframe.winfo_children(): 
+        for child in self.mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
         feet_entry.focus()
-        root.bind("<Return>", self.calculate)
-        
+
+
     def calculate(self, *args):
         try:
             value = float(self.feet.get())
@@ -45,6 +101,7 @@ def rungui(txque, rxque):
        rxque is the queue of received data"""
 
     root = Tk()
-    FeetToMeters(root)
+    applicationframe = appframe(root)
+    screen = MessageScreen(txque, rxque, root, applicationframe)
     root.mainloop()
     txque.put(None)
