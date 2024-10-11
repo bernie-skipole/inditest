2b835db2471059852244088b46424e3c8501a91a
Bernard Czenkusz <bernie@skipole.co.uk>
2024-10-11 20:07:59 +0100

    queclient.py done

--- docexamples/queclient.py
+++ docexamples/queclient.py
@@ -6,7 +6,7 @@
 """
 
 
-import queue, threading, collections
+import threading, collections
 
 from indipyclient.queclient import runqueclient
 
@@ -15,8 +15,11 @@ from indipyclient.queclient import runqueclient
 # rxque giving received data
 rxque = collections.deque(maxlen=1)
 
+# length of one so old measurements are bumped off
+# and rxque[0] is always the latest.
+
 # txque transmit data
-txque = queue.Queue(maxsize=4)
+txque = collections.deque()
 
 # run the queclient in its own thread
 clientthread = threading.Thread(target=runqueclient, args=(txque, rxque))
@@ -33,16 +36,16 @@ while True:
         try:
             # get latest data received
             rxitem = rxque[0]
-        except IndexError:
+            temperature = rxitem.snapshot["Thermostat"]["temperaturevector"]["temperature"]
+        except (IndexError, KeyError):
             print("Nothing received yet")
             continue
-        temperature = rxitem.snapshot["Thermostat"]["temperaturevector"]["temperature"]
         timestamp = rxitem.timestamp.isoformat(sep='T')
-        print(f"{timestamp} Temperature: {timestamp}")
+        print(f"{timestamp} Temperature: {temperature}")
     elif value == "q" or value == "Q":
         break
 
 # When the loop ends, transmit a None value to shut down the queclient
-txque.put(None)
+txque.append(None)
 # and wait for the clientthread to stop
 clientthread.join()
