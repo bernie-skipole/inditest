536afac5f2d3902972de748a885f094d3038ad17
Bernard Czenkusz <bernie@skipole.co.uk>
2024-10-18 16:51:26 +0100

    Added consclient.py

---
+++ docexamples/consclient.py
@@ -0,0 +1,21 @@
+"""Script dedicated to a terminal connecting
+   to a set host and port"""
+
+import asyncio
+
+# stop anything going to the screen
+import logging
+logger = logging.getLogger()
+logger.addHandler(logging.NullHandler())
+
+from indipyclient.console import ConsoleClient
+
+# create a ConsoleClient calling host and port
+client = ConsoleClient(indihost="raspberrypi", indiport=7624)
+
+try:
+    # Starts the client, creates the console screens
+    asyncio.run(client.asyncrun())
+finally:
+    # clear curses setup
+    client.console_reset()
