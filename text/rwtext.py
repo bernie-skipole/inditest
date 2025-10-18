8b43e15d3db1597f1929f1ac3439d916e1dc4b5f
Bernard Czenkusz <bernie@skipole.co.uk>
2025-10-18 14:01:24 +0100

    Testing delayed processing

--- text/rwtext.py
+++ text/rwtext.py
@@ -6,17 +6,34 @@
 # ///
 
 
-"TextVector with ten members, reading and writing"
+"""TextVector with ten members, reading and writing
+   Together with another vector controlling a longer 15 second task"""
 
 import asyncio
 
 import indipydriver as ipd
 
 
+async def delay_instrument(vector, value):
+    """When the driver receives a vector and value for this instrument, this is called
+       as a background task, simulating an instrument taking some time to process the data.
+       When done, this instrument updates the vector, and sends it back to the client"""
+    # simulate instrument processing time
+    await asyncio.sleep(15)
+    # having processed, set the vector value, and transmit this new value
+    vector['tovalue'] = value
+    await vector.send_setVector(message="Delayed value sent back")
+
+
+
+
+
 class RWDriver(ipd.IPyDriver):
     """IPyDriver is subclassed here
-       It has one rw TextVector with ten members
-       Also sends text every ten seconds"""
+       It has a rw TextVector with ten members
+       with new text sent every ten seconds
+       and a single vector which on receiving new data, only responds
+       after 15 seconds, to test timeout functions"""
 
     async def rxevent(self, event):
         """On receiving data."""
@@ -33,6 +50,32 @@ class RWDriver(ipd.IPyDriver):
                 # transmit the vector back to client to confirm received
                 await event.vector.send_setVector()
 
+            case ipd.newTextVector(devicename='rwtext',
+                                   vectorname='timeoutvector'):
+                # get the received value
+                value = event.get('tovalue')
+                if value is None:
+                    # member name not recognised
+                    return
+
+                # send this event.vector and value to an instrument which processes the
+                # value, then sends an acknowledgement back to the client
+                # In this example, this is simulated by the 'delay_instrument' function
+
+                # create a task, in this case run 'delay_instrument'
+                task = asyncio.create_task(delay_instrument(event.vector, value))
+
+                # Add task to the 'background_tasks' set. This creates a strong reference.
+                background_tasks = self.driverdata['background_tasks']
+                background_tasks.add(task)
+                # To prevent keeping references to finished tasks forever,
+                # make each task remove its own reference from the set after
+                # completion:
+                task.add_done_callback(background_tasks.discard)
+                # As this task is now running in the background, this rxevent method
+                # can now end, and will not block further functionality of the driver.
+
+
     async def hardware(self):
         "Every ten seconds, send new text"
         vector = self['rwtext']['rwvector']
@@ -47,7 +90,7 @@ class RWDriver(ipd.IPyDriver):
 def make_driver():
     "Returns an instance of the driver"
 
-    # create ten members
+    # create a rw vector with ten members
     members = []
 
     for m in range(10):
@@ -63,12 +106,36 @@ def make_driver():
                                state = "Ok",
                                textmembers = members)
 
-    # create a device with this vector
+
+    # Create a vector with one member that has a 15 second time delay
+    # and a suggested timeout of 8 seconds. This should cause a client
+    # to show Busy after eight seconds, and alert after ten.
+
+    timeoutmember = ipd.TextMember( name="tovalue",
+                                    label="Timeout Member",
+                                    membervalue="Starting value" )
+
+    timeoutvector = ipd.TextVector( name = 'timeoutvector',
+                                    label = "Timeout Vector",
+                                    group = 'RW TEXT',
+                                    perm = "rw",
+                                    state = "Ok",
+                                    textmembers = [timeoutmember])
+
+    # set a timeout of eight seconds
+    timeoutvector.timeout = "8"
+
+
+    # create a device with these vectors
     rwtext = ipd.Device( devicename="rwtext",
-                         properties=[rwvector] )
+                         properties=[rwvector, timeoutvector] )
 
     # Create the Driver, containing this Device
-    driver = RWDriver( rwtext )
+
+    # This driver operates an instrument by running background tasks
+    # store these tasks in the driver to stop them being garbage collected
+    background_tasks = set()
+    driver = RWDriver( rwtext, background_tasks=background_tasks )
 
     # and return the driver
     return driver
